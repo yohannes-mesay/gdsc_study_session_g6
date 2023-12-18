f76912e6690d13cfc70d26bbbd3c952cee69489a
yohannes mesay <johnnymesay@gmail.com>
2023-12-18 18:04:29 +0300

    first commit

---
+++ list_members/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+const App = () => {
+  const names = [
+    "abebe",
+    "kebede",
+    "yohannes",
+  ];
+  return (
+    <div className="list">
+      <ul class="myul">
+        <h2>members </h2>
+        {names.map((name) => (
+          <li>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
