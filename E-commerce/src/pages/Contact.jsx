0fa7bc37e43f940f13ad6be10099c5f912a44040
yohannes mesay <johnnymesay@gmail.com>
2024-01-02 21:05:42 +0300

    updated

---
+++ E-commerce/src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+// src/components/Contact.jsx
+import React from 'react';
+
+const Contact = () => {
+  return <h2>Contact Page</h2>;
+};
+
+export default Contact;
---
+++ E-commerce/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+// src/components/NotFound.jsx
+import React from 'react';
+
+const NotFound = () => {
+  return <h2>404 - Not Found</h2>;
+};
+
+export default NotFound;
