409268e9521c5580e92127698f6838cf43aac94b
yohannes mesay <johnnymesay@gmail.com>
2024-01-10 09:45:03 +0300

    Updated

--- E-commerce/src/components/Header.jsx
+++ E-commerce/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import '../index.css'
 const Header = () => {
   return (
     <div className="container">
@@ -8,8 +8,27 @@ const Header = () => {
         <div className="logo">
           <Link to="/" className="brand">
             WSP
-          </Link>   
+          </Link>
         </div>
+        <div className="nav-nav-pi.ls">
+          <Link to="/" className="brand">
+            Home
+          </Link>
+
+          <Link to="about" className="brand">
+            About
+          </Link>
+          <Link to="products" className="brand">
+            Products
+          </Link>
+          <Link to="/" className="brand">
+            Home
+          </Link>
+          <Link to="/" className="brand">
+            Home
+          </Link>
+        </div>
+
       </div>
     </div>
   );
